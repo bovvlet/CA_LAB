608151e56749677498573754c295a730f1685706
bovvlet <bovvlet@users.noreply.github.com>
2020-10-02 15:08:37 +0300

    update

---
+++ week 5/ex4/ex4.v
@@ -0,0 +1,22 @@
+module ex3
+	(
+		input s,
+		input r,
+		output q,
+		output q_n
+	);
+	assign q = ~ ( r | q_n ) ;
+	assign q_n = ~ ( s | q ) ;
+endmodule
+
+module ex4
+(
+	input clk,
+	input d,
+	output q,
+	output q_n
+);
+	wire r = ~d & clk;
+	wire s = d & clk ;
+	ex3 ex3(s, r, q, q_n) ;
+endmodule
